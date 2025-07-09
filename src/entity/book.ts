67db4c28494c2f4a04333638413d9f1db719d50d
Iulia Mihaicuta <Iulia.Mihaicuta@softwire.com>
2025-07-09 16:28:10 +0300

    add author to book

--- src/entity/book.ts
+++ src/entity/book.ts
@@ -3,11 +3,19 @@ export class Book {
     public title: string;
     public isbn: string;
     public nr_copies: number;
+    public author: string | null;
 
-    constructor(id: number, title: string, isbn: string, nr_copies: number) {
+    constructor(
+        id: number,
+        title: string,
+        isbn: string,
+        nr_copies: number,
+        author: string | null,
+    ) {
         this.id = id;
         this.title = title;
         this.isbn = isbn;
         this.nr_copies = nr_copies;
+        this.author = author;
     }
 }
